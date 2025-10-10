be995cb37c30b5b469c8a698048c3250693dd5b5
Zefanya Ecklezia Saragih <zefanyaecklesia.s@gmail.com>
2025-10-10 23:35:47 +0700

    main

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  // load .env sesuai mode (development/production)
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
+    base: "/", // 🟢 penting: pastikan path build benar di Vercel
     build: {
       outDir: "dist", // folder hasil build
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"), // 🟢 supaya import "@/..." bisa dipakai
+      },
     },
     define: {
       DELCOM_BASEURL: JSON.stringify(env.VITE_DELCOM_BASEURL || ""),
